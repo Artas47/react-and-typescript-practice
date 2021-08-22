fbbc8513e2d77037e9fb29002b2e849c22b00de7
ARTAS <artasss47@gmail.com>
2021-08-22 14:20:09 +0200

    dog-facts fix

--- examples/16-dog-facts/src/Application.tsx
+++ examples/16-dog-facts/src/Application.tsx
@@ -1,18 +1,36 @@
 import * as React from 'react';
 import { fetchDogFacts, DogFactType } from './dog-facts';
 
-const Form = () => {
+type FormProps = {
+  onSubmit: (n: number) => void;
+};
+
+const Form = ({ onSubmit }: FormProps) => {
+  const [number, setNumber] = React.useState<number>(0);
+
+  const handleNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNumber(+e.target.value);
+  };
+
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
+        onSubmit(number);
       }}
     >
       <div className="fact-input">
         <label htmlFor="number-of-facts">Number of Dog Facts</label>
-        <input type="number" value="3" min="1" max="10" id="number-of-facts" />
+        <input
+          type="number"
+          value={number}
+          onChange={handleNumberChange}
+          min="1"
+          max="10"
+          id="number-of-facts"
+        />
       </div>
-      <input type="submit" value="Fetch Dog Facts" />
+      <input disabled={!number} type="submit" value="Fetch Dog Facts" />
     </form>
   );
 };
@@ -27,10 +45,22 @@ const Fact = ({ fact }: { fact: string }) => {
 };
 
 const Application = () => {
+  const [dogFacts, setDogFacts] = React.useState<DogFactType[]>([]);
+
+  const onSubmit = async (n: number) => {
+    const res = await fetchDogFacts(n);
+    setDogFacts(res);
+  };
+
   return (
     <main>
-      <Form />
-      <section></section>
+      <Form onSubmit={onSubmit} />
+      <section>
+        {!!dogFacts.length &&
+          dogFacts.map((fact) => {
+            return <Fact fact={fact.fact} />;
+          })}
+      </section>
     </main>
   );
 };
